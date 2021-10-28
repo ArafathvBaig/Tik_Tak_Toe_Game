3af396eac0199ac8965b8d63b12997f1d9da95a3
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-28 12:35:25 +0530

    Adding File to Branch-2

--- src/com/TicTacToeGame/TicTacToeGame.java
+++ src/com/TicTacToeGame/TicTacToeGame.java
@@ -1,8 +1,13 @@
 package com.TicTacToeGame;
 
+import java.util.Scanner;
+
 public class TicTacToeGame 
 {
 	char board[] = new char[10];
+	char player;
+	char computer;
+	char choice;
 	public void newBoard()
 	{
 		for(int i=1; i<board.length; i++)
@@ -10,9 +15,25 @@ public class TicTacToeGame
 			board[i]=' ';
 		}
 	}
+	public void choose()
+	{
+		Scanner sc=new Scanner(System.in);
+		choice = sc.next().charAt(0);
+		if (choice=='x')
+		{
+			player='x';
+			computer='o';
+		}
+		else
+		{
+			player='o';
+			computer='x';
+		}
+	}
 	public static void main(String[] args)
 	{
 		TicTacToeGame obj1=new TicTacToeGame();
 		obj1.newBoard();
+		obj1.choose();
 	}
 }
